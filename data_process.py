874c1ae2571e456fac7a231c33923fa37cd6807c
yafei <yafei.feng@gmail.com>
2017-04-05 18:06:15 +0800

    Edit data_process.py, use pre_trie and post_trie to compute information entropy of left and right.

--- data_process.py
+++ data_process.py
@@ -33,50 +33,65 @@ class Process(object):
         self._gramFile = gramFile
         self._candidateFile = candidateFile
         self._scoreFile = scoreFile
-        self._trie = trie.StringTrie(separator=Process.SEP)
-        self._trieFile = 'GramTrie'
-        self._cache = {}
+        """pre_trie"""
+        self._pretrie = trie.StringTrie(separator=Process.SEP)
+        self._pretrieFile = 'PreGramTrie'
+        self._precache = {}
+        """post_trie"""
+        self._posttrie = trie.StringTrie(separator=Process.SEP)
+        self._posttrieFile = 'PostGramTrie'
+        self._postcache = {}
         assert os.path.exists(gramFile), "GramFile %s not exists" % gramFile
         assert os.path.exists(candidateFile), "CandidateFile %s not exists" % candidateFile
 
     def buildTrie(self):
         """Generate StringTrie"""
-        Count = 0
         with open(self._gramFile) as fd:
             for line in fd:
                 key = line.strip()
-                self._trie.setdefault(key, 0)
-                self._trie[key] += 1
-                Count += 1
-                if Count % 100000 == 0:
-                    print "Count: ", Count
-        #with open(self._trieFile, 'w') as fd:
-        #   pickle.dump(self._trie, fd)
+                self._pretrie.setdefault(key, 0)
+                self._pretrie[key] += 1
+
+                #I love to embrace, prefix(love to) = {embrace,...}, postfix(love to) = {I,...}
+                try:
+                    #last line in gram-3 has only 2 column
+                    preword, midword, postword = line.strip().split(Process.SEP)
+                except:
+                    continue
+                key = Process.SEP.join([midword,postword,preword])
+                self._posttrie.setdefault(key, 0)
+                self._posttrie[key] += 1
+
+        #with open(self._pretrieFile, 'w') as fd:
+        #   pickle.dump(self._pretrie, fd)
 
     def lazySum(self, key):
-        if not self._cache.has_key(key):
-            self._cache[key] = sum(self._trie.itervalues(prefix=key))
-        return self._cache[key]
+        if not self._precache.has_key(key):
+            self._precache[key] = sum(self._pretrie.itervalues(prefix=key))
+        return self._precache[key]
 
+    """
+    Use trie to generate score for candidate.
+    pmi(x, y) = log p(xy)/(p(x) * p(y))
+    """
     def generateScore(self, file=None):
-        """Use trie to generate score for candidate.
-        pmi(x, y) = log p(xy)/(p(x) * p(y))
-        """
+
         if file:
             with open(file, 'rb') as fd:
-                self._trie = pickle.load(fd)
-        totalFreq = sum(self._trie.itervalues())
+                self._pretrie = pickle.load(fd)
+        totalFreq = sum(self._pretrie.itervalues())
         with open(self._scoreFile, 'w') as outFd:
             with open(self._candidateFile) as fd:
                 for line in fd:
                     words = line.strip().split('\t')
                     if len(words) < 2 or any(map(lambda word:len(word)<2, words)):
                         continue
-                    #The last word in the whole text is lost frequency
-                    if not self._trie.has_key(words[1]):
+                    try:
+                        # The last word in the whole text is lost frequency
+                        XFreq = self.lazySum(words[0])
+                        YFreq = self.lazySum(words[1])
+                    except:
                         continue
-                    XFreq = self.lazySum(words[0])
-                    YFreq = self.lazySum(words[1])
                     XYFreq = self.lazySum(line.strip())
                     # frequences filter
                     if XYFreq < 2 or XYFreq > 24:
@@ -84,10 +99,30 @@ class Process(object):
                     PX = XFreq * 1.0 / totalFreq
                     PY = YFreq * 1.0 / totalFreq
                     PXY = XYFreq * 1.0 / totalFreq
-                    score = math.log(PXY/PX/PY, 2) * XYFreq
-                    #print "Freq:", XFreq, YFreq, XYFreq
-                    result = "{0}\t{1:.2f}\n".format(line.strip(), score)
+                    pmi = math.log(PXY/PX/PY, 2) * XYFreq
+                    result = "{0}\t{1:.2f}\n".format(line.strip(), pmi)
                     outFd.write(result)
+    """
+    use pre_trie and post_trie to compute information entropy of left and right.
+    H(x) = -sum(P(x)logP(x))
+    """
+    def computeEntropy(self, candidatekey):
+        H_right = 0
+        sumright = 0
+        for item in self._pretrie.iteritems(prefix=candidatekey):
+            sumright += item[1]
+        for item in self._pretrie.iteritems(prefix=candidatekey):
+            probility_x = 1.0*item[1]/sumright
+            H_right += -probility_x*math.log(probility_x, 2)
+        H_left = 0
+        sumleft = 0
+        for item in self._posttrie.iteritems(prefix=candidatekey):
+            sumleft += item[1]
+        for item in self._posttrie.iteritems(prefix=candidatekey):
+            probility_x = 1.0*item[1]/sumleft
+            H_left += -probility_x*math.log(probility_x, 2)
+        print "\t".join(map(str, [H_right, sumright]))
+        print "\t".join(map(str, [H_left, sumleft]))
 
 def test():
     gramNumber = 3
@@ -103,11 +138,11 @@ def test():
     tu.reset()
     key1 = 'acronyms\tand\tterms'
     key2 = 'not\tplaced\tin'
-    print prop._trie.has_key(key1)
-    print prop._trie.has_key(key2)
+    print prop._pretrie.has_key(key1)
+    print prop._pretrie.has_key(key2)
     tu.elapsedSeconds()
     tu.reset()
-    print sum(prop._trie.itervalues(prefix='additional'))
+    print sum(prop._pretrie.itervalues(prefix='additional'))
     tu.elapsedSeconds()
     tu.reset()
     prop.generateScore()
@@ -125,16 +160,23 @@ def test1():
     prop.generateScore('GramTrie')
     tu.elapsedSeconds()
 
+def test_pre_post_trie():
+    gramNumber = 3
+    candidateNumber = 2
+    gramFile = r'C:\Users\yafe\Downloads\%s-gram.txt' % str(gramNumber)
+    candidateFile = r'C:\Users\yafe\Downloads\%s-candidate.txt' % str(candidateNumber)
+    scoreFile = r'C:\Users\yafe\Downloads\%s-score.txt' % str(candidateNumber)
+    prop = Process(gramFile, candidateFile, scoreFile)
+    prop.buildTrie()
+    print "pre_trie------------"
+    for item in prop._pretrie.iteritems(prefix="information\ttheory"):
+        print item
+    print "post_trie-----------"
+    for item in prop._posttrie.iteritems(prefix="information\ttheory"):
+        print item
+    #prop.generateScore()
+    prop.computeEntropy("information\ttheory")
 
 if __name__ == '__main__':
-    '''
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-u', action='store_true', default=False)
-    opts = parser.parse_args(sys.argv[1:])
-    if opts.u:
-        test1()
-    else:
-        test()
-    '''
-    test()
+    test_pre_post_trie()
 
